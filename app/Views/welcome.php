cb48e8e8f23d1afe40192992d54f1c6f496f506d
Sentgine <sentgine@gmail.com>
2024-06-26 20:49:17 +0800

    Updated welcome page.

--- app/Views/welcome.php
+++ app/Views/welcome.php
@@ -2,15 +2,26 @@
 layout('layout/app');
 param('title', $title);
 ?>
-<div class="min-h-screen flex flex-col justify-center py-6 px-6">
-    <div class="max-w-3xl mx-auto text-center mb-16">
-        <div class="flex justify-center">
-            <img src="<?= url('hikari-logo.png') ?>" alt="Hikari Logo" class="h-28 w-28">
+<div class="flex items-center justify-center h-screen">
+    <div class="grid grid-cols-1 gap-1 text-center mb-10">
+        <div class="flex items-center justify-center gap-5">
+            <img src="<?= url('hikari-logo.png') ?>" class="gradient w-[70px] mt-4 rounded-lg" />
+            <h1 class="text-8xl font-bold">Hikari</h1>
         </div>
-        <h1 class="text-6xl font-bold mb-6">Hikari</h1>
-        <p class="text-lg mb-8">The PHP micro-framework designed for simplicity and speed.</p>
-        <div class="flex justify-center">
-            <a href="https://hikari.sentgine.com/" class="bg-white text-blue-700 hover:bg-blue-200 hover:text-blue-900 font-bold py-3 px-6 rounded-lg mr-4">Read the docs!</a>
+        <p class="mt-2 italic">&quot;If the only tool you have is a hammer, you tend to see every problem as a nail.&quot;</p>
+        <div class="flex items-center justify-between mt-5">
+            <a href="<?= url('docs') ?>" class="flex items-center gap-3 hover:underline hover:underline-offset-2">
+                <svg class="size-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+                    <path d="M64 464c-8.8 0-16-7.2-16-16V64c0-8.8 7.2-16 16-16H224v80c0 17.7 14.3 32 32 32h80V448c0 8.8-7.2 16-16 16H64zM64 0C28.7 0 0 28.7 0 64V448c0 35.3 28.7 64 64 64H320c35.3 0 64-28.7 64-64V154.5c0-17-6.7-33.3-18.7-45.3L274.7 18.7C262.7 6.7 246.5 0 229.5 0H64zm56 256c-13.3 0-24 10.7-24 24s10.7 24 24 24H264c13.3 0 24-10.7 24-24s-10.7-24-24-24H120zm0 96c-13.3 0-24 10.7-24 24s10.7 24 24 24H264c13.3 0 24-10.7 24-24s-10.7-24-24-24H120z" />
+                </svg>
+                <span>Docs</span>
+            </a>
+            <a href="https://github.com/sentgine/hikari" class="flex items-center gap-3 hover:underline hover:underline-offset-2">
+                <svg class="size-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512">
+                    <path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 .3-6.2 2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 3.9 1.6 1 3.6 .7 4.3-.7 .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 .7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z" />
+                </svg>
+                <span>Github</span>
+            </a>
         </div>
     </div>
 </div>
